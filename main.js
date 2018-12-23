4cff6bbc4850b00cae1f5ac3c90482b9f6755937
Habashh <zaid85j@gmail.com>
2018-12-23 15:29:02 +0300

    DOS-create save and delete

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 let comments_list = null;
-let comments_list2;
 let comment_form = null;
 let comment_input = null;
 let comment_remove=null;
-var tab = [];
-var index;
+let delete_comment;
+
 
 document.addEventListener("DOMContentLoaded", (e) => {
     comments_list = document.querySelector("#comments_list");
-    comments_list2 = document.querySelectorAll("#comments_list li");
     comment_form = document.querySelector("#comment_form");
     comment_input = document.querySelector("#comment_body");
     comment_remove = document.querySelector(".comment");
@@ -28,24 +26,40 @@ document.addEventListener("DOMContentLoaded", (e) => {
             comment_input.value = "";
         }
     })
-// remove comment
 
-comments_list.addEventListener('click', function() {
-  deleteComment();
-removeComment()
-   });
 })
 function addComment(text) {
     let template = document.createElement("li");
+    let template2 = document.createElement("INPUT");
+    template2.setAttribute("type","submit");
+    template2.setAttribute("value","");
+    template2.setAttribute("id","delete_comment");
+    template2.setAttribute("onclick","deleteComment() ; removeComment();");
     template.classList.add("comment");
     template.innerText = text;
     comments_list.appendChild(template);
+    template.appendChild(template2);
 }
-
+var tab = [], index;
 function removeComment(){
-     
-    comments_list.removeChild(comments_list.childNodes[0]);
-        }
+    
+    delete_comment = document.querySelectorAll("#comments_list li");
+    
+    for(var i = 0; i < delete_comment.length; i++){
+       tab.push(delete_comment[i].innerHTML);
+     }
+    
+    for(var i = 0; i < delete_comment.length; i++)
+    {
+       delete_comment[i].onclick = function(){
+           
+           index = tab.indexOf(this.innerHTML);
+          
+           comments_list.removeChild(comments_list.childNodes[index]);
+        };
+    }
+   
+       }
    
 function saveComment(text) {
     let comments = localStorage.getItem("local_comments");
